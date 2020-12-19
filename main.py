f3a92d88f26af0860e75dcd79ae97d1357ac386a
Kunal <Kunal@myhost.local>
2020-12-19 23:03:37 +0530

    realtime face detection

--- main.py
+++ main.py
@@ -7,8 +7,11 @@ import tensorflow.keras as keras
 from  tensorflow.keras.layers import *
 
 def predict_class(roi_img, model):
+    #print(roi_img,'roi_img')i
+    
     emotion_dict = {0: "Angry", 1: "Disgusted", 2: "Fearful", 3: "Happy", 4: "Neutral", 5: "Sad", 6: "Surprised"}
     cropped_img = np.expand_dims(np.expand_dims(cv2.resize(roi_img, (48,48)), -1),0)
+    cropped_img = cropped_img / 255.0
     pred_class = model.predict_classes(cropped_img)
     return emotion_dict[pred_class[0]]
 
---
+++ realtime_face_detector.py
@@ -0,0 +1,56 @@
+import cv2
+import onnxruntime as ort
+import argparse, copy
+import numpy as np
+import matplotlib.pyplot as plt
+import tensorflow as tf
+import os
+
+#This is for mac you can comment it if you want
+os.environ['KMP_DUPLICATE_LIB_OK']='True'
+
+face_detector_onnx = "/Users/Kunal/Downloads/version-RFB-320.onnx"
+face_detector      = ort.InferenceSession(face_detector_onnx)
+
+def face_box(image):
+    height   = image.shape[0]
+    width    = image.shape[1]
+    
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image = cv2.resize(image, (320, 240))
+
+    image_mean = np.array([127, 127, 127])
+    image = (image - image_mean) / 128
+    image = np.transpose(image, [2, 0, 1])
+    image = np.expand_dims(image, axis=0)
+    image = image.astype(np.float32)
+
+    input_name = face_detector.get_inputs()[0].name
+    confidences, boxes = face_detector.run(None, {input_name: image})
+
+
+    new_boxes = boxes[0]
+    indices = tf.image.non_max_suppression(new_boxes, confidences[0][:,1], 1, 0.5 )
+
+    selected_boxes = tf.gather(new_boxes, indices)
+    selected_boxes = selected_boxes.numpy()
+
+    selected_boxes[:,0] *= width
+    selected_boxes[:,1] *= height
+    selected_boxes[:,2] *= width
+    selected_boxes[:,3] *= height
+
+
+    x_min, y_min, x_max, y_max = selected_boxes[0]
+    return x_min, y_min, x_max, y_max
+
+cap = cv2.VideoCapture(0)
+while cap.isOpened():
+    _, img = cap.read()
+    x_min, y_min, x_max, y_max = face_box(img)
+    cv2.rectangle(img, (x_min,y_min), (x_max, x_max), (255, 0 , 0), 3)
+    
+    cv2.imshow('img', img)
+    
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
